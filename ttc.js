ff6cbc730c6c123850cc0c401bf60e8a5c4e7cbf
SkyNeverKnow <130295632+SkyNeverKnow@users.noreply.github.com>
2023-04-09 20:59:31 +0700

    Create ttc.js

---
+++ ttc.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rainbow Effect</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			height: 100vh;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+	</style>
+</head>
+<body>
+	<h1 id="rainbow">Rainbow Effect</h1>
+	<script src="rainbow.js"></script>
+</body>
+</html>
